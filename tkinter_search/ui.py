6b3fcf3ad1b9071ca5d44e45660f2105e15a70b1
Kushami Neko <missing10630@gmail.com>
2020-02-16 09:19:23 +0900

    plot scatter with matplotlib tkinter backend

--- tkinter_search/ui.py
+++ tkinter_search/ui.py
@@ -1,6 +1,19 @@
+import gc
 import json
 import math
 import tkinter
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from matplotlib import font_manager as fm
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from scipy import stats
+
+from preprocess import clean_data, test_data_na
+
+matplotlib.use("tkagg")
 
 
 class SearchField:
@@ -28,15 +41,26 @@ class SearchField:
 
     @staticmethod
     def _new_search_entry(window, column, row, width=25):
-        entry = tkinter.Entry(window, width=width)
-        entry.grid(column=column, row=row)
+        frame = tkinter.Frame(window)
+        frame.grid(
+            column=column,
+            row=row,
+            sticky=tkinter.W + tkinter.E + tkinter.S + tkinter.N,
+        )
+
+        entry = tkinter.Entry(frame, width=width)
+        entry.pack(side=tkinter.LEFT, fill=tkinter.BOTH, expand=tkinter.YES)
 
         return entry
 
     @staticmethod
     def _new_list_box(window, column, row, height=35):
         frame = tkinter.Frame(window)
-        frame.grid(column=column, row=row, sticky=tkinter.W + tkinter.E)
+        frame.grid(
+            column=column,
+            row=row,
+            sticky=tkinter.W + tkinter.E + tkinter.S + tkinter.N,
+        )
 
         scrollbarX = tkinter.Scrollbar(frame, orient=tkinter.HORIZONTAL)
         scrollbarY = tkinter.Scrollbar(frame, orient=tkinter.VERTICAL)
@@ -56,7 +80,7 @@ class SearchField:
         scrollbarY.config(command=listbox.yview)
         scrollbarY.pack(side=tkinter.RIGHT, fill=tkinter.Y)
 
-        listbox.pack(side=tkinter.LEFT, fill=tkinter.BOTH, expand=1)
+        listbox.pack(side=tkinter.LEFT, fill=tkinter.BOTH, expand=tkinter.YES)
 
         return listbox
 
@@ -165,14 +189,26 @@ class SearchField:
 
 
 class MyApp:
-    def __init__(self, statistic, title="Statistic"):
+    def __init__(self, statistic, df, title="Statistic"):
+
+        self._statistic = statistic
+        self._df = df
+
         self._window = tkinter.Tk()
         self._window.title(title)
 
-        self._statistic = statistic
+        self._root = tkinter.Frame(self._window)
+        self._root.pack(fill=tkinter.BOTH, expand=tkinter.YES)
+        self._root.grid_columnconfigure(0, weight=1)
+        self._root.grid_columnconfigure(1, weight=1)
 
-        self._src = SearchField(self._window, 0, 0, "src")
-        self._tar = SearchField(self._window, 1, 0, "tar")
+        self._root.grid_rowconfigure(0, weight=1)
+        self._root.grid_rowconfigure(1, weight=35)
+        self._root.grid_rowconfigure(2, weight=1)
+        self._root.grid_rowconfigure(3, weight=1)
+
+        self._src = SearchField(self._root, 0, 0, "src")
+        self._tar = SearchField(self._root, 1, 0, "tar")
 
         self._src.set_selection_cb(self._selection_cb)
         self._tar.set_selection_cb(self._selection_cb)
@@ -182,29 +218,54 @@ class MyApp:
         self._src.set_listbox_items(self._statistic.keys())
         self._tar.set_listbox_items(self._statistic.keys())
 
+        self._info = tkinter.Frame(self._root)
+        self._info.grid(
+            column=0,
+            row=2,
+            columnspan=2,
+            sticky=tkinter.W + tkinter.E + tkinter.S + tkinter.N,
+        )
+
+        self._info.grid_rowconfigure(0, weight=1)
+        self._info.grid_rowconfigure(1, weight=1)
+        self._info.grid_rowconfigure(2, weight=1)
+        self._info.grid_rowconfigure(3, weight=1)
+        # self._info.grid_rowconfigure(4, weight=1)
+
         self._a_var = tkinter.StringVar()
-        self._a_label = tkinter.Label(self._window, textvariable=self._a_var)
-        self._a_label.grid(column=0, row=2, columnspan=2, sticky=tkinter.W)
+        self._a_label = tkinter.Label(self._info, textvariable=self._a_var)
+        self._a_label.grid(column=0, row=0, columnspan=2, sticky=tkinter.W)
 
         self._a_var.set("A:")
 
         self._b_var = tkinter.StringVar()
-        self._b_label = tkinter.Label(self._window, textvariable=self._b_var)
-        self._b_label.grid(column=0, row=3, columnspan=2, sticky=tkinter.W)
+        self._b_label = tkinter.Label(self._info, textvariable=self._b_var)
+        self._b_label.grid(column=0, row=1, columnspan=2, sticky=tkinter.W)
 
         self._b_var.set("B:")
 
         self._p_var = tkinter.StringVar()
-        self._p_label = tkinter.Label(self._window, textvariable=self._p_var)
-        self._p_label.grid(column=0, row=4, columnspan=2, sticky=tkinter.W)
+        self._p_label = tkinter.Label(self._info, textvariable=self._p_var)
+        self._p_label.grid(column=0, row=2, columnspan=2, sticky=tkinter.W)
 
         self._p_var.set("P:")
 
         self._tau_var = tkinter.StringVar()
-        self._tau_label = tkinter.Label(self._window, textvariable=self._tau_var)
-        self._tau_label.grid(column=0, row=5, columnspan=2, sticky=tkinter.W)
+        self._tau_label = tkinter.Label(self._info, textvariable=self._tau_var)
+        self._tau_label.grid(column=0, row=3, columnspan=2, sticky=tkinter.W)
 
         self._tau_var.set("TAU:")
+
+        self._plot_button = tkinter.Button(
+            self._root, text="plot", height=2, command=lambda: self._plot_chart(),
+        )
+
+        self._plot_button.grid(
+            column=0,
+            row=3,
+            columnspan=2,
+            sticky=tkinter.W + tkinter.E + tkinter.S + tkinter.N,
+        )
 
     def _significant_cb(self):
         src = self._src.selected
@@ -212,7 +273,7 @@ class MyApp:
             items = []
 
             for k, v in self._statistic[src].items():
-                if float(v["p"]) < 0.05:
+                if float(v["p"]) <= 0.05:
                     items.append(k)
 
             return items
@@ -222,7 +283,7 @@ class MyApp:
         if src is not None and src != "":
             indexes = []
             for i, v in enumerate(self._tar.filtered_items):
-                if float(self._statistic[src][v]["p"]) < 0.05:
+                if float(self._statistic[src][v]["p"]) <= 0.05:
                     indexes.append(i)
 
             self._tar.set_listbox_items_significant(indexes)
@@ -235,8 +296,8 @@ class MyApp:
         src = self._src.selected
         tar = self._tar.selected
 
-        self._a_var.set(f"A: {src}")
-        self._b_var.set(f"B: {tar}")
+        self._a_var.set("A: {}".format(src))
+        self._b_var.set("B: {}".format(tar))
 
         if self._statistic is None:
             return
@@ -249,8 +310,47 @@ class MyApp:
                 p = vk.get("p", math.nan)
                 tau = vk.get("tau", math.nan)
 
-                self._p_var.set(f"P: {str(p)}")
-                self._tau_var.set(f"TAU: {str(tau)}")
+                self._p_var.set("P: {}".format(str(p)))
+                self._tau_var.set("TAU: {}".format(str(tau)))
+
+    def _plot_chart(self):
+        src = self._src.selected
+        tar = self._tar.selected
+
+        x = self._df[src]
+        y = self._df[tar]
+
+        s, i, _, _, _ = stats.linregress(x, y)
+        xl = np.linspace(x.min(), x.max())
+
+        win = tkinter.Toplevel()
+        win.wm_title("plot")
+
+        fig, ax = plt.subplots(figsize=(12, 6))
+        win.protocol("WM_DELETE_WINDOW", lambda: self._clear_plot(win, fig, ax))
+        win.bind("<Escape>", lambda event: self._clear_plot(win, fig, ax))
+
+        ax.scatter(x, y, s=35)
+        ax.plot(xl, xl * s + i, color="r")
+
+        prop = fm.FontProperties(fname="fonts/Kosugi/Kosugi-Regular.ttf", size=12)
+
+        ax.set_xlabel(src, fontproperties=prop)
+        ax.set_ylabel(tar, fontproperties=prop)
+
+        fig.tight_layout()
+
+        canvas = FigureCanvasTkAgg(fig, master=win)
+        canvas.get_tk_widget().grid(row=0, column=0)
+
+        canvas.draw()
+
+    def _clear_plot(self, win, fig, ax):
+        win.destroy()
+        plt.cla()
+        plt.clf()
+
+        gc.collect()
 
     @property
     def window(self):
@@ -260,8 +360,25 @@ class MyApp:
 if __name__ == "__main__":
     stat = None
 
+    print("reading statistic.json")
+
     with open("statistic.json", "r") as f:
         stat = json.load(f)
 
-    app = MyApp(stat)
+    print("reading data.csv")
+
+    df = pd.read_csv("data.csv")
+
+    df = df[:19]
+    df = df.astype(np.float64)
+
+    df = clean_data(df)
+
+    print("testing dataframe.....")
+
+    test_data_na(df)
+
+    print("ready to launch the program.....")
+
+    app = MyApp(stat, df)
     app.window.mainloop()
